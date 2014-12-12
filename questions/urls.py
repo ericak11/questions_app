4c4280810f0bc2d3a7342e564c0212fe630cc043
ericak11 <kantor.erica@gmail.com>
2014-12-12 17:03:22 -0500

    added list of companys

--- questions/urls.py
+++ questions/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
       url(r'^register/$', views.register, name='register'),
       url(r'^logout/$', views.user_logout, name='logout'),
       url(r'^companys/$', views.companys, name='companys'),
+      # url(r'^companys/(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
 )
