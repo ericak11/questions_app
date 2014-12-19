308e50b863a9b46e661c787cb5aa3bba1600aecf
ericak11 <kantor.erica@gmail.com>
2014-12-19 12:03:33 -0500

    updated requirements.txt dependancies

--- questions/views.py
+++ questions/views.py
@@ -61,52 +61,25 @@ def user_login(request):
 
 
 def register(request):
-    # Like before, get the request's context.
     context = RequestContext(request)
-
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
     registered = False
-
-    # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
-        # If the two forms are valid...
         if user_form.is_valid():
-            # Save the user's form data to the database.
             user = user_form.save()
-
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
             user.set_password(user.password)
             user.save()
-
-            # Update our variable to tell the template registration was successful.
             registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
         else:
             print user_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
     else:
         user_form = UserForm()
-
-    # Render the template depending on the context.
     return render(request, 'register/register.html', {
             'user_form': user_form, 'registered': registered})
 
 @login_required
 def user_logout(request):
-    # Since we know the user is logged in, we can now just log them out.
     logout(request)
-
-    # Take the user back to the homepage.
     return HttpResponseRedirect('/')
 
 @login_required
@@ -117,107 +90,53 @@ def companys(request):
 
 @login_required
 def add_question(request, pk):
-    # Like before, get the request's context.
     context = RequestContext(request)
     registered = False
-    # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
         question_form = QuestionForm(data=request.POST, initial={'user': request.user, 'company': pk})
-        # If the two forms are valid...
         if question_form.is_valid():
-            # Save the user's form data to the database.
             question_form.instance.user = request.user
             question_form.instance.company = Company.objects.get(id=pk)
             question_form.save()
-
-
-            # Update our variable to tell the template registration was successful.
             registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
         else:
             print question_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
     else:
         question_form = QuestionForm()
-
-    # Render the template depending on the context.
     return render(request, 'question/new.html', {
             'question_form': question_form, 'registered': registered})
 
 @login_required
 def add_answer(request, pk):
-    # Like before, get the request's context.
     context = RequestContext(request)
     registered = False
-    # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
         answer_form = AnswerForm(data=request.POST, initial={'user': request.user, 'question': pk})
-        # If the two forms are valid...
         if answer_form.is_valid():
-            # Save the user's form data to the database.
             answer_form.instance.user = request.user
             answer_form.instance.question = Question.objects.get(id=pk)
             answer_form.save()
-
-
-            # Update our variable to tell the template registration was successful.
             registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
         else:
             print answer_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
     else:
         answer_form = AnswerForm()
-
-    # Render the template depending on the context.
     return render(request, 'answer/new.html', {
             'answer_form': answer_form, 'registered': registered, 'question': Question.objects.get(id=pk) })
 
 @login_required
 def add_company(request):
-    # Like before, get the request's context.
     context = RequestContext(request)
     registered = False
-    # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
         company_form = CompanyForm(data=request.POST, initial={'user': request.user})
-        # If the two forms are valid...
         if company_form.is_valid():
-            # Save the user's form data to the database.
             company_form.instance.user = request.user
             company_form.save()
-
-
-            # Update our variable to tell the template registration was successful.
             registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
         else:
             print company_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
     else:
         company_form = CompanyForm()
-
-    # Render the template depending on the context.
     return render(request, 'company/new.html', {
             'company_form': company_form, 'registered': registered})
